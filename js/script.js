8b2e4f1960aa1af7c19ca734e755f9ecd5f635a0
Atte-Oksanen <atteoksanen@outlook.com>
2023-05-09 16:04:28 +0300

    fixed navigation bug

--- js/script.js
+++ js/script.js
@@ -64,6 +64,9 @@ function clickEventDelegator(event) {
         case "cancelSettings":
             toggleSettings();
             break;
+        case "closeSettings":
+            toggleSettings();
+            break;
         case "closeShare":
             toggleShare();
             break;
